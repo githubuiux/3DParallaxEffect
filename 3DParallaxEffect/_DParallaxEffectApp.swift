2348a879f22cebffc5431784b01f9868f89ad161
githubuiux <uiuxista@gmail.com>
2023-08-01 13:26:41 +0300

    Initial Commit

---
+++ 3DParallaxEffect/_DParallaxEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DParallaxEffectApp.swift
+//  3DParallaxEffect
+//
+//  Created by Levent on 1.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct _DParallaxEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
